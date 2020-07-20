6311afd9ce8b5b893a293b515648989c9b6e67dc
Tiago Matinho <tiagomartinho97@gmail.com>
2020-07-20 18:19:01 +0100

    Update

--- Código/CURRENT V5.1/path.h
+++
@@ -1,7 +0,0 @@
-#include "min_heap.h"
-
-
-void path_insert(struct flight_list* list, struct list_node* node);
-struct list_node* path_remove(struct flight_list* list);
-
-void print_path(struct flight_list* list);
--- README.md
+++ README.md
@@ -3,5 +3,3 @@
 Trabalho final de EDA2 2018-2019
 
 Aplicação do algoritmo de Djikstra.
-
-TODO: Reduzir o tempo de execução.
